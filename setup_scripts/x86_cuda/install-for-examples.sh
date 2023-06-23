2120b778f12ed8a507cfcdf97358da37beb63374
Carlton <carlton@latentai.com>
2023-06-23 22:17:14 +0000

    Modified docker install script for cleaner torch install

--- setup_scripts/x86_cuda/install-for-examples.sh
+++ setup_scripts/x86_cuda/install-for-examples.sh
@@ -15,16 +15,19 @@ apt install -y libopencv-dev
 apt install -y libjpeg-dev libopenblas-dev libopenmpi-dev libomp-dev
 apt install -y liblre-cuda liblre-dev
 
-
+OWD=$PWD
+mkdir ~/.torch-apps
+cd ~/.torch-apps
 wget https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcu118.zip
-unzip libtorch-cxx11-abi-shared-with-deps-2.0.1+cu118.zip 
+unzip libtorch-cxx11-abi-shared-with-deps-2.0.1+cu118.zip
+cd $OWD
 
 apt install -y libjpeg-dev zlib1g-dev libavcodec-dev libpng-dev
 git clone --branch v0.13.1 https://github.com/pytorch/vision torchvision
 cd torchvision
 mkdir build 
 cd build
-cmake -DCMAKE_PREFIX_PATH=/path/to/libtorch -DWITH_CUDA=ON .. 
+cmake -DCMAKE_PREFIX_PATH=~/.torch-apps/libtorch -DWITH_CUDA=ON .. 
 make -j12
 make install
 ldconfig
