62562f637a27f07c5962eda9dbb25a3e7678a551
EBAD URRAHMAN SYED <syedebadurrahman@gmail.com>
2023-11-10 15:31:02 +0500

    2.10 (#6)
    * effdet_gpp
    
    * all detectors
    
    ---------
    
    Co-authored-by: ebadsyed <ebad.u.syed@gmail.com>

--- detectors/efficientdet/cpp_inference/application.cpp
+++ detectors/efficientdet/cpp_inference/application.cpp
@@ -40,14 +40,12 @@ int main(int argc, char *argv[]) {
   // WarmUp Phase 
   model.WarmUp(1);
 
-  std::vector<at::Tensor> results;
-
   // Run pre, inference and post processing x iterations
-  for (int j = 1; j < iterations; j++) {
+  for (int j = 0; j < iterations; j++) {
     
     auto imageInput{ReadImage(imgPath)};
     cv::Scalar background(124, 116, 104);
-    imageInput = resizeAndCenterImage(imageInput, cv::Size (model.input_width,model.input_height), background);
+    imageInput = ResizeImage(imageInput, model.input_width,model.input_height);
 
     // Preprocessing
     t_preprocessing.start();
@@ -59,52 +57,43 @@ int main(int argc, char *argv[]) {
     model.InferOnce(processed_image.data);
     t_inference.stop();
 
-    /// Post Processing
-    results.clear();
+    /*      Post Processing      */
 
-    // Data Transform
+    // Convert DLTensor to at::Tensor
     t_op_transform.start();
-    auto outputs = convert_to_atTensor(model.tvm_outputs);
-    auto clo_bx_in_cls = get_top_classes_and_boxes(outputs);
+    auto outputs = convert_to_atTensor(model.tvm_outputs[0]);
     t_op_transform.stop();
 
-    // Generate Anchors
-    t_anchors_gen.start();
-    torch::Tensor anchors = generate_anchors(model.input_height, model.input_width, 3, 7, 3,clo_bx_in_cls["box_outputs_all_after_topk"].device().type());
-    t_anchors_gen.stop();
+    auto tensors_ = ssd_tensors(outputs[0],model.input_width,model.input_height);
 
-    int batch_size = outputs[0].sizes()[0];
+    // NMS from Torchvision 
+    auto result = batched_nms_coordinate_trick(tensors_["boxes"],tensors_["scores"],tensors_["classes"],0.45);
 
-    // Decode Boxes
-    for (int i = 0; i < batch_size; i++)
-    {
-      t_decoding.start();
-      auto class_out = clo_bx_in_cls["cls_outputs_all_after_topk"][i];
-      auto box_out = clo_bx_in_cls["box_outputs_all_after_topk"][i];
-      auto indices = clo_bx_in_cls["indices_all"][i];
-      auto classes = clo_bx_in_cls["classes_all"][i];
+    // TopK Filter
+    result = result.slice(0,0,100);
 
-      torch::Tensor anchor_boxes = anchors.index({indices, at::indexing::Slice(0, at::indexing::None)});
-      auto box_out_decoded = decode_box_outputs(box_out, anchor_boxes, true);
-      t_decoding.stop();
-     
-      // NMS
-      t_nms.start();
-      auto detections = vision_nms(box_out_decoded,class_out.sigmoid().squeeze(1),classes);
-      results.emplace_back(detections);
-      t_nms.stop();
-    }
+    tensors_["boxes"] = tensors_["boxes"].index({result});
+    tensors_["classes"] = tensors_["classes"].index({result,at::indexing::None});
+    tensors_["scores"] = tensors_["scores"].index({result,at::indexing::None});
+    auto detections = at::cat({tensors_["boxes"],tensors_["scores"],tensors_["classes"]},1);
+
+    // Confidence Threshold Filter 
+    auto filtered_detections = at::where(detections.index({"...",4}) > 0.3);
+    detections = detections.index({filtered_detections[0]});
+
+    std::cout << detections << std::endl;
+ 
   }
 
-  print_detections(results[0]);
-  draw_boxes(results[0], imgPath,model.input_width, model.input_height);
+  // print_detections(results[0]);
+  // draw_boxes(results[0], imgPath,model.input_width, model.input_height);
 
-  std::cout << "Average Preprocessing Time: " << t_preprocessing.averageElapsedMilliseconds() << " ms" << std::endl;
-  std::cout << "Average Inference Time: " << t_inference.averageElapsedMilliseconds() << " ms" << std::endl;
-  std::cout << "Average Output Data Manipulation Time: " << t_op_transform.averageElapsedMilliseconds() << " ms" << std::endl;
-  std::cout << "Average Anchors Generation Time: " << t_anchors_gen.averageElapsedMilliseconds() << " ms" << std::endl;
-  std::cout << "Average Decoding Time: " << t_decoding.averageElapsedMilliseconds() << " ms" << std::endl;
-  std::cout << "Average NMS Time: " << t_nms.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average Preprocessing Time: " << t_preprocessing.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average Inference Time: " << t_inference.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average Output Data Manipulation Time: " << t_op_transform.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average Anchors Generation Time: " << t_anchors_gen.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average Decoding Time: " << t_decoding.averageElapsedMilliseconds() << " ms" << std::endl;
+  // std::cout << "Average NMS Time: " << t_nms.averageElapsedMilliseconds() << " ms" << std::endl;
 
 
 }
