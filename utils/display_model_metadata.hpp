601ecaa0240d75954cb6717c9672e1587abce07f
AchachayJurado <Natalia.jurado@latentai.com>
2023-11-10 11:31:40 +0100

    Adapt lre c++ apis (#7)
    Co-authored-by: ebad syed <ebad.u.syed@gmail.com>

--- utils/display_model_metadata.hpp
+++ utils/display_model_metadata.hpp
@@ -2,10 +2,27 @@
 #include <iomanip>
 #include <iostream>
 
-void PrintModelMetadata(LreModel &model){
-  std::cout << std::setw(8) << "----------------------------------------\n";
-  std::cout << std::left << std::setw(8) <<  "Device" << " | " << "Flag    " << " | " << "UUID\n";
-  std::cout << std::setw(8) << "--------" << " | " << "--------" << " | " << "------------------\n";
-  std::cout << std::setw(8) << getEnumString(model.GetModelMetadata().device) << " | " <<  getEnumString(model.GetModelMetadata().flag) << "     | " << model.GetModelMetadata().uuid << "\n";
-  std::cout << std::setw(8) << "----------------------------------------\n";
+void PrintModelMetadata(LRE::LatentRuntimeEngine &model){
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Unique ID:\n";
+  std::cout << std::setw(12) << model.getModelID() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Creation Date:\n";
+  std::cout << std::setw(12) << model.getModelMetadata().creation_date << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Device:\n";
+  std::cout << std::setw(12) << model.getDeviceType() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Precision: \n";
+  std::cout << std::setw(12) << model.getModelPrecision() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Input Shapes: \n";
+  std::cout << std::setw(12) << model.getInputShapes() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "Output Shapes: \n";
+  std::cout << std::setw(12) << model.getOutputShapes() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
+  std::cout << std::left << std::setw(12) << "SDK version: \n";
+  std::cout << std::setw(12) << model.getSDKVersion() << "\n";
+  std::cout << std::setw(12) << "----------------------------------------\n";
 }
