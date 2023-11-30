43206c26a7da44e06a2a4810c14b58f035b18a35
AchachayJurado <natalia.juradoes@gmail.com>
2023-11-30 08:34:13 -0500

    WIP mobnet inference example

--- detectors/mobilenet_ssd/python_inference/infer.py
+++ detectors/mobilenet_ssd/python_inference/infer.py
@@ -13,11 +13,14 @@ import sys
 import datetime
 from argparse import ArgumentParser
 from pathlib import Path
+from pylre import LatentRuntimeEngine
+import torchvision.transforms as transforms
+import torch as T
 
 def main():
 
     parser = ArgumentParser(description="Run inference")
-    parser.add_argument("--lre_object", type=str, default=".", help="Path to LRE object directory.")
+    parser.add_argument("--path_to_model", type=str, default=".", help="Path to LRE object directory.")
     parser.add_argument(
         "--input_image",
         type=str,
@@ -32,44 +35,67 @@ def main():
     )
     
     args = parser.parse_args()
-    sys.path.append(str(Path(args.lre_object) / "latentai.lre"))
+    sys.path.append(str(Path(args.path_to_model)))
+
+    from processors import general_detection_postprocessor
     
-    import latentai_runtime
     import numpy as np
     from PIL import Image
-    from preprocessors.factory import get_preprocessor
-    from postprocessors.factory import get_postprocessor
-    from representations.boundingboxes.boundingbox import BoundingBox
-    from representations.boundingboxes.utils import BBFormat
-    
-    model = latentai_runtime.Model()
 
+    
+    model_runtime = LatentRuntimeEngine(str(Path(args.path_to_model) / "modelLibrary.so"))
+    print(model_runtime.get_metadata())
+    
     labels = load_labels(args.labels)
 
     image = Image.open(args.input_image)    
-    data = model._preprocess([image])
-    
-    inference = model.infer(data)
-    
-    output = model._postprocess(inference, args.input_image)
 
-    rgb_img = image.convert("RGB")
-    out_im = np.array(cv2.cvtColor(np.array(rgb_img), cv2.COLOR_BGR2RGB))
-    threshold = 0.3
-    for bb in output:
-        for i in range(0,len(bb)):
-            if bb[i].get_confidence() > threshold:
-                out_im = plot_one_box(
-                    bb[i].get_coordinates(BBFormat.absolute_xyx2y2, image_size=rgb_img.size),
-                    out_im,
-                    color=(255, 0, 0),
-                    label=labels[bb[i].get_class_id()],
-                )
+    layout_shapes = get_layout_dims(model_runtime.input_layouts, model_runtime.input_shapes)
+    image_size = (layout_shapes[0].get('H'), layout_shapes[0].get('W'))
+    print(image_size)
     
-    p = os.path.splitext(args.input_image)
-    output_filename = f"{p[0]}-{datetime.datetime.now()}{p[1]}"
-    cv2.imwrite(output_filename, out_im)
-    print("Annotated image written to", output_filename)
+    resize_transform = transforms.Resize(image_size)
+    resized_image = resize_transform(image)
+    normalize_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    # Apply the normalization transformation
+    resized_image_normalized = normalize_transform(resized_image)
+    
+    # Run inference
+    model_runtime.infer(resized_image_normalized)
+    
+    # Get outputs as a list of PyDLPack
+    outputs = model_runtime.get_outputs()
+    output = outputs[0]
+    outputdl = T.from_dlpack(output)
+    
+    # import pdb; pdb.set_trace()
+    device = model_runtime.device_type    
+    output = general_detection_postprocessor.postprocess(outputdl, max_det_per_image=10, prediction_confidence_threshold=0.5, iou_threshold=0.2, height=image_size[0], width=image_size[1], model_output_format="ssd", device=device)
+    print(output)
+
+
+    # output = model._postprocess(inference, args.input_image)
+
+    # rgb_img = image.convert("RGB")
+    # out_im = np.array(cv2.cvtColor(np.array(rgb_img), cv2.COLOR_BGR2RGB))
+    # threshold = 0.3
+    # for bb in output:
+    #     for i in range(0,len(bb)):
+    #         if bb[i].get_confidence() > threshold:
+    #             out_im = plot_one_box(
+    #                 bb[i].get_coordinates(BBFormat.absolute_xyx2y2, image_size=rgb_img.size),
+    #                 out_im,
+    #                 color=(255, 0, 0),
+    #                 label=labels[bb[i].get_class_id()],
+    #             )
+    
+    # p = os.path.splitext(args.input_image)
+    # output_filename = f"{p[0]}-{datetime.datetime.now()}{p[1]}"
+    # cv2.imwrite(output_filename, out_im)
+    # print("Annotated image written to", output_filename)
 
 
 def load_labels(path):
@@ -103,6 +129,23 @@ def plot_one_box(box, img, color, label=None, line_thickness=None):
 
     return img
 
+def get_layout_dims(layout_list, shape_list):
+    if len(layout_list) != len(shape_list):
+        raise ValueError("Both input lists should have the same number of elements.")
+    
+    result = []
+    
+    for i in range(len(layout_list)):
+        layout_str = layout_list[i]
+        shape_tuple = shape_list[i]
+        
+        if len(layout_str) != len(shape_tuple):
+            raise ValueError(f"Length of layout string does not match the number of elements in the shape tuple for input {i}.")
+        
+        layout_dict = {letter: number for letter, number in zip(layout_str, shape_tuple)}
+        result.append(layout_dict)
+    
+    return result
 
 if __name__ == "__main__":
     main()
