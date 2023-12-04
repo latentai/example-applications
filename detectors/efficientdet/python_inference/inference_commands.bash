e443417a7fafd105f67ba17ff11e168b0d19781a
axaxaj <natalia.jurado@latentai.com>
2023-12-04 23:43:57 +0000

    Fuxes in script

--- detectors/efficientdet/python_inference/inference_commands.bash
+++ detectors/efficientdet/python_inference/inference_commands.bash
@@ -12,18 +12,18 @@ INT8_PACKAGE=~/models/efficientdet_d0/x86_64_cuda/Int8-package
 
 if [ -v MODEL_PATH ];
 then
-    FLOAT32_PACKAGE=$MODEL_PATH/Float32-package
-    INT8_PACKAGE=$MODEL_PATH/Int8-package
+    FLOAT32_PACKAGE=$MODEL_PATH/Float32-compile
+    INT8_PACKAGE=$MODEL_PATH/Int8-optimize
 fi
 
 echo "FP32..."
 mkdir -p $FLOAT32_PACKAGE/trt-cache
-TVM_TENSORRT_CACHE_DIR=$FLOAT32_PACKAGE/trt-cache python3 infer.py --lre_object $FLOAT32_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
+TVM_TENSORRT_CACHE_DIR=$FLOAT32_PACKAGE/trt-cache python3 infer.py --path_to_model $FLOAT32_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
 
 echo "FP16..."
 mkdir -p $FLOAT32_PACKAGE/trt-cache
-TVM_TENSORRT_CACHE_DIR=$FLOAT32_PACKAGE/trt-cache TVM_TENSORRT_USE_FP16=1 python3 infer.py --lre_object $FLOAT32_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
+TVM_TENSORRT_CACHE_DIR=$FLOAT32_PACKAGE/trt-cache TVM_TENSORRT_USE_FP16=1 python3 infer.py --path_to_model $FLOAT32_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
 
 echo "INT8..."
 mkdir -p $INT8_PACKAGE/trt-cache
-TVM_TENSORRT_CACHE_DIR=$INT8_PACKAGE/trt-cache TVM_TENSORRT_USE_INT8=1 TRT_INT8_PATH=~/.latentai/LRE/.model/.activations/ python3 infer.py --lre_object $INT8_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
+TVM_TENSORRT_CACHE_DIR=$INT8_PACKAGE/trt-cache TVM_TENSORRT_USE_INT8=1 TRT_INT8_PATH=~/.latentai/LRE/.model/.activations/ python3 infer.py --path_to_model $INT8_PACKAGE --input_image ../../../sample_images/bus.jpg --labels ../../../labels/coco.txt
