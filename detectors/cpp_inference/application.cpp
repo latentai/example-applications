ef6ade3ab49ad80879dec40599453e3bc3b30177
AchachayJurado <natalia.juradoes@gmail.com>
2023-12-09 17:43:55 -0500

    Run for detectors

--- detectors/cpp_inference/application.cpp
+++ detectors/cpp_inference/application.cpp
@@ -30,7 +30,6 @@ int main(int argc, char *argv[]) {
   int iterations = params.iterations;
   std::string imgPath = params.img_path;
   at::Tensor detections{};
-
   
   // Model Factory
   LRE::LatentRuntimeEngine model(model_binary);
@@ -43,11 +42,6 @@ int main(int argc, char *argv[]) {
   for (int j = 0; j < iterations; j++) {
     
     auto imageInput{ReadImage(imgPath)};
-
-    // effdet //resize //  pre effdet // effdet tensor
-    // ssd   // resize // pre effdet // ssd tensor
-    // yolo // resize and center // yolo tensor
-    // nanodet // resize // pre effdet // yolo tensor // 0.4
 
     // Preprocessing
     t_preprocessing.start();
@@ -67,12 +61,10 @@ int main(int argc, char *argv[]) {
     t_inference.stop();
 
     // Post Processing
-
-    // Convert DLTensor to at::Tensor
+    t_postprocessing.start();
     t_op_transform.start();
+    // Convert DLTensor to at::Tensor
     auto outputs = convert_to_atTensor(model.getOutputs()[0]);
-    t_op_transform.stop();
-
     #if EFFICIENTDET 
      auto tensors_ = effdet_tensors(outputs[0]);
     #elif NANODET || YOLO
@@ -80,9 +72,12 @@ int main(int argc, char *argv[]) {
     #elif MOBNETSSD
      auto tensors_ = ssd_tensors(outputs[0], model.input_width, model.input_height);
     #endif
+    t_op_transform.stop();
 
     // NMS from Torchvision 
+    t_nms.start();
     auto result = batched_nms_coordinate_trick(tensors_["boxes"],tensors_["scores"],tensors_["classes"], IOU_THRESHOLD);
+    t_nms.stop();
 
     // TopK Filter
     result = result.slice(0,0,100);
@@ -95,17 +90,17 @@ int main(int argc, char *argv[]) {
     // Confidence Threshold Filter 
     auto filtered_detections = at::where(detections.index({"...",4}) > CONFIDENCE_THRESHOLD);
     detections = detections.index({filtered_detections[0]});
+    t_postprocessing.stop();
 
   }
   print_detections(detections);
   draw_boxes(detections, imgPath,model.input_width, model.input_height);
 
-  // std::cout << "Average Preprocessing Time: " << t_preprocessing.averageElapsedMilliseconds() << " ms" << std::endl;
-  // std::cout << "Average Inference Time: " << t_inference.averageElapsedMilliseconds() << " ms" << std::endl;
-  // std::cout << "Average Output Data Manipulation Time: " << t_op_transform.averageElapsedMilliseconds() << " ms" << std::endl;
-  // std::cout << "Average Anchors Generation Time: " << t_anchors_gen.averageElapsedMilliseconds() << " ms" << std::endl;
-  // std::cout << "Average Decoding Time: " << t_decoding.averageElapsedMilliseconds() << " ms" << std::endl;
-  // std::cout << "Average NMS Time: " << t_nms.averageElapsedMilliseconds() << " ms" << std::endl;
+  std::cout << "Average Preprocessing Time: " << t_preprocessing.averageElapsedMilliseconds() << " ms" << std::endl;
+  std::cout << "Average Inference Time: " << t_inference.averageElapsedMilliseconds() << " ms" << std::endl;
+  std::cout << "Average Output Data Manipulation Time: " << t_op_transform.averageElapsedMilliseconds() << " ms" << std::endl;
+  std::cout << "Average NMS Time: " << t_nms.averageElapsedMilliseconds() << " ms" << std::endl;
+  std::cout << "Average Postprocessing Time: " << t_postprocessing.averageElapsedMilliseconds() << " ms" << std::endl;
 
 
 }
--- detectors/cpp_inference/inference_commands.bash
+++ detectors/cpp_inference/inference_commands.bash
@@ -7,19 +7,15 @@
 
 #!/bin/bash
 
-NANODET=0
-MOBNETSSD=0
-EFFICIENTDET=0
-YOLO=1
+ARCH=aarch64_cuda_xavier_jp4
+PATH_TO_DETECTOR_MODELS=/home/dev/models/recipe_hub
 
-FLOAT32_MODEL=~/models/detector/x86_64_cuda/Float32-compile
-INT8_MODEL=~/models/detector/x86_64_cuda/Int8-optimize
-
-if [ -v MODEL_PATH ];
-then
-    FLOAT32_MODEL=$MODEL_PATH/Float32-compile
-    INT8_MODEL=$MODEL_PATH/Int8-optimize
-fi
+models=(NANODET MOBNETSSD EFFICIENTDET YOLO YOLO) # supported detector models
+paths=($PATH_TO_DETECTOR_MODELS/nanodet/$ARCH
+       $PATH_TO_DETECTOR_MODELS/ssdn/$ARCH
+       $PATH_TO_DETECTOR_MODELS/efficientdetn/$ARCH
+       $PATH_TO_DETECTOR_MODELS/yolov5n/$ARCH
+       $PATH_TO_DETECTOR_MODELS/yolov8/$ARCH)
 
 # Current known issue: These applications require a different libtorch
 # (libtorch-cxx11-abi-shared-with-deps-2.0.1%2Bcu118.zip) installed that
@@ -36,34 +32,50 @@ else
 fi
 echo $TORCH_PATH
 
-# Generate the config.h
-echo "#ifndef CONFIG_H" > include/config.h
-echo "#define CONFIG_H" >> include/config.h
-echo "#define CONFIDENCE_THRESHOLD 0.30"  >> include/config.h # 0.3 normally, just for nanodet 0.45
-echo "#define IOU_THRESHOLD 0.45"  >> include/config.h
+if [ ${#models[@]} -ne ${#paths[@]} ]; then
+    echo "Error: The number of models and paths do not match."
+    exit 1
+fi
 
-echo "#define NANODET $NANODET" >> include/config.h
-echo "#define MOBNETSSD $MOBNETSSD" >> include/config.h
-echo "#define EFFICIENTDET $EFFICIENTDET" >> include/config.h
-echo "#define YOLO $YOLO" >> include/config.h
-echo "#endif // CONFIG_H" >> include/config.h
+for i in "${!models[@]}"; do
+    model="${models[$i]}"
+    path="${paths[$i]}"
 
-# Compile
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH=$TORCH_PATH ..
-make -j 8
-cd ..
+    model = 1
+
+    echo "Running code for $model with path $path"
+    FLOAT32_MODEL=$path/Float32-compile
+    INT8_MODEL=$path/Int8-optimize
+
+    # Generate the config.h
+    echo "#ifndef CONFIG_H" > include/config.h
+    echo "#define CONFIG_H" >> include/config.h
+    echo "#define CONFIDENCE_THRESHOLD 0.30"  >> include/config.h # 0.3 normally, just for nanodet 0.45
+    echo "#define IOU_THRESHOLD 0.45"  >> include/config.h
+    echo "#define $model 1" >> include/config.h
+    echo "#endif // CONFIG_H" >> include/config.h
+
+    # Compile
+    mkdir build
+    cd build
+    cmake -DCMAKE_PREFIX_PATH=$TORCH_PATH ..
+    make -j 8
+    cd ..
 
 
-# FP32
-mkdir -p $FLOAT32_MODEL/trt-cache/
-TVM_TENSORRT_CACHE_DIR=$FLOAT32_MODEL/trt-cache/ ./build/bin/application $FLOAT32_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
+    # FP32
+    mkdir -p $FLOAT32_MODEL/trt-cache/
+    TVM_TENSORRT_CACHE_DIR=$FLOAT32_MODEL/trt-cache/ ./build/bin/application $FLOAT32_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
 
-# FP16
-mkdir -p $FLOAT32_MODEL/trt-cache/
-TVM_TENSORRT_CACHE_DIR=$FLOAT32_MODEL/trt-cache/ TVM_TENSORRT_USE_FP16=1 ./build/bin/application $FLOAT32_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
+    # FP16
+    mkdir -p $FLOAT32_MODEL/trt-cache/
+    TVM_TENSORRT_CACHE_DIR=$FLOAT32_MODEL/trt-cache/ TVM_TENSORRT_USE_FP16=1 ./build/bin/application $FLOAT32_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
 
-# INT8
-mkdir -p $INT8_MODEL/trt-cache/
-TVM_TENSORRT_CACHE_DIR=$INT8_MODEL/trt-cache/ TVM_TENSORRT_USE_INT8=1 TRT_INT8_PATH=$INT8_MODEL/.activations/ ./build/bin/application $INT8_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
+    # INT8
+    mkdir -p $INT8_MODEL/trt-cache/
+    TVM_TENSORRT_CACHE_DIR=$INT8_MODEL/trt-cache/ TVM_TENSORRT_USE_INT8=1 TRT_INT8_PATH=$INT8_MODEL/.activations/ ./build/bin/application $INT8_MODEL/modelLibrary.so 10 ../../sample_images/bus.jpg
+
+done
+
+
+
