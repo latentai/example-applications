5fab9645fa3c2a0dee826c66fff97f1c923bb7c5
sachille <sachille.atapattu@latentai.com>
2023-12-18 11:39:50 -0500

    Cleanup post-processor

--- detectors/python_inference/utils/detector_postprocessor.py
+++ detectors/python_inference/utils/detector_postprocessor.py
@@ -11,7 +11,7 @@ import itertools
 
 
 # input is size: batch_size x num_anchors_after_top_k x [x_min_abs, y_min_abs, x_max_abs, y_max_abs, top_class_index, top_class_score]
-def post_process_efficientdet_format_for_leip(
+def post_process_efficientdet(
     input, iou_threshold, max_det_per_image, prediction_confidence_threshold
 ):
     """
@@ -38,48 +38,13 @@ def post_process_efficientdet_format_for_leip(
         # Confidence threshold based filtering
         mask = detections[:,4] > prediction_confidence_threshold
         batch_detections.append(detections[mask])
+    
     return batch_detections
-
-
-
-def transform_into_leip_representation(batch_detections, output_format, height, width):
-    from representations.boundingboxes.boundingbox import BoundingBox
-    from representations.boundingboxes.utils import BBFormat
-    postprocessed_batch = []
-    convert_coco80_to_coco91 = (output_format == 'nanodet')
-    coco80_to_coco91_map = [x for x in range(1, 91) if x not in [12, 26, 29, 30, 45, 66, 68, 69, 71, 83]] if convert_coco80_to_coco91 else None
-    for sample_detections in batch_detections:
-        bbs = []
-        for bbox in sample_detections:
-            class_id = coco80_to_coco91_map[bbox[5].int() - 1] if coco80_to_coco91_map else bbox[5].int()
-            bbs.append(
-                BoundingBox(
-                    class_id=class_id,
-                    coords=bbox[:4],
-                    image_size=(width, height),
-                    confidence=bbox[4],
-                    bb_format=BBFormat.pascal_voc,
-                )
-            )
-        postprocessed_batch.append(bbs)
-    return postprocessed_batch
-
-
-def transform_into_AF_representation(batch_detections):
-    from af.core.utils.annotations import BoundingBox2d as bbox2d
-    postprocessed_batch = []
-    for sample_detections in batch_detections:
-        sample_detections = sample_detections.cpu()
-        bbs = []
-        for bbox in sample_detections:
-            bbs.append(bbox2d.from_format('pascal_voc', bbox[:4], label=int(bbox[5]), confidence=bbox[4]))
-        postprocessed_batch.append(bbs)
-    return postprocessed_batch
 
 
 # input is size: batch_size x num_anchors x [x_min_norm,  y_min_norm,  x_max_norm,  y_max_norm, class_0_score, class_1_score ... class_n_score]
 # class_0_score is additional background class which is later ignored
-def transform_ssd_to_efficientdet(input, width, height, **kwargs):
+def transform_ssd_to_efficientdet(input, width, height):
     # Prepare the scaling tensor
     scale_tensor = torch.tensor([width, height, width, height], device=input.device)
 
@@ -98,8 +63,6 @@ def transform_ssd_to_efficientdet(input, width, height, **kwargs):
     return transformed_detections
 
 
-
-
 def transform_yolo_nanodet_to_efficientdet(input):
     """ Translates the decoded nanodet outputs to the format the GPP expects, via a set of vectorized transformations on GPU
     Args:
@@ -109,16 +72,19 @@ def transform_yolo_nanodet_to_efficientdet(input):
         """
     # Extract bounding boxes
     bboxes = input[:, :, :4]
+    
     # Extract class scores and find top classes and scores
     class_scores = input[:, :, 4:]
     top_classes = torch.argmax(class_scores, dim=2)
     top_scores = torch.max(class_scores, dim=2).values
+    
     # Stack transformed detections
     transformed_detections = torch.cat([bboxes, top_classes.unsqueeze(2).float(), top_scores.unsqueeze(2)], dim=2)
+    
     return transformed_detections
 
 
-def postprocess(input, **kwargs):
+def postprocess(input, output_format, input_size, max_det_per_image, prediction_confidence_threshold, iou_threshold):
     """
     Args
         input: List of outputs for a batch, where each output is
@@ -133,20 +99,8 @@ def postprocess(input, **kwargs):
         A Sequence(batch size). Each item in that sequence is a Sequence
         of LEIP representations, either Labels or BoundingBoxes.
     """
-    max_det_per_image = int(kwargs["max_det_per_image"])
-    prediction_confidence_threshold = float(kwargs["prediction_confidence_threshold"])
-    iou_threshold = float(kwargs["iou_threshold"])
-    height = int(kwargs["height"])
-    width = int(kwargs["width"])
-    output_format = kwargs["output_format"]
-    deploy_env = kwargs["deploy_env"]
-    # Deal with different input format when model is ingested via SDK
-    if isinstance(input, list) and isinstance(input[0], ndarray):
-        assert len(input) == 1, f"there is more than one model output, but the general postprocessor you are using expects a single model output"
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # print("Moving model outputs to CUDA for postprocessing because it is available" if torch.cuda.is_available() else "postprocessing on CPU")
-        input = torch.from_numpy(input[0]).to(device) # only the first element of the list is needed, it also requires numpy -> torch.tensor conversion
-        deploy_env = 'leip'
+    height = input_size[0]
+    width = input_size[1]
     
     # Perform necesary vectorized transformations to adapt decoded model outputs
     if output_format == "ssd":
@@ -155,13 +109,7 @@ def postprocess(input, **kwargs):
         input = transform_yolo_nanodet_to_efficientdet(input)
 
     # Do the Postprocessing: top K filtering -> NMS -> confidence based filtering
-    batch_detections = post_process_efficientdet_format_for_leip(input, iou_threshold, max_det_per_image, prediction_confidence_threshold)
+    batch_detections = post_process_efficientdet(input, iou_threshold, max_det_per_image, prediction_confidence_threshold)
     
-    if deploy_env == 'leip':
-        postprocessed_batch = transform_into_leip_representation(batch_detections, output_format, height, width)
-    elif deploy_env == 'torch':
-        postprocessed_batch = batch_detections
-    else:
-        postprocessed_batch = transform_into_AF_representation(batch_detections)
-    return postprocessed_batch
+    return batch_detections
 
