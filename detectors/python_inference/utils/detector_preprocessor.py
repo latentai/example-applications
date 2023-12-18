c9339ad5952110464f6e2ebb402792502a091bf1
sachille <sachille.atapattu@latentai.com>
2023-12-18 15:26:00 -0500

    Enable padding

--- detectors/python_inference/utils/detector_preprocessor.py
+++ detectors/python_inference/utils/detector_preprocessor.py
@@ -12,29 +12,39 @@ def torch_preprocess_transforms(image, input_transform, input_size):
 
     ## Transforms to resize and pad
     resize_transform = transforms.Resize(input_size)
+    image_size = image.size
+    width_height_image = image_size[0]/image_size[1]
+    width_height_expected = input_size[0]/input_size[1]
+    if width_height_image > width_height_expected:
+        new_image_size = (input_size[0], input_size[1] / width_height_image)
+        resize_var = int(input_size[1] / width_height_image)
+    else:
+        new_image_size = (input_size[0] * width_height_image, input_size[1])
+        resize_var = int(input_size[0] * width_height_image)
+    filler = tuple(int(a - b) for a, b in zip(input_size, new_image_size))
     resize_center_color1_transform = transforms.Compose([
-        transforms.Resize(input_size)
-        # transforms.Resize(384),  # Equivalent to LongestMaxSize
-        # transforms.Pad ((0, 0, 512-384, 0), fill=(124, 116, 104), padding_mode='constant'),  # Equivalent to PadIfNeeded
+        # transforms.Resize(input_size)
+        transforms.Resize(resize_var),  # Equivalent to LongestMaxSize
+        transforms.Pad ((0, 0, filler[0], filler[1]), fill=(124, 116, 104), padding_mode='constant'),  # Equivalent to PadIfNeeded
     ])
     ## Resize is like shortestmaxsize, so I'm forcing 1080 to be 512 by resizing with 512/1080*810.
     ## Since height then needs padding, I do 512-384. This does not center things.
     ## Still the boxes are off.
     resize_center_color2_transform = transforms.Compose([
-        transforms.Resize(input_size)
-        # transforms.Resize(480),  # Equivalent to LongestMaxSize
-        # transforms.Pad((80, 0, 80, 0), fill=(128, 128, 128), padding_mode='constant'),  # Equivalent to PadIfNeeded
+        # transforms.Resize(input_size)
+        transforms.Resize(resize_var),  # Equivalent to LongestMaxSize
+        transforms.Pad((int(filler[0]/2), int(filler[1]/2), int(filler[0]/2), int(filler[1]/2)), fill=(128, 128, 128), padding_mode='constant'),  # Equivalent to PadIfNeeded
     ])
     ## padding makes it look wrong. Why?
     # if input is padded, the visual image should also be padded
     
     ## Transforms to normalize and tensorize.
-    float_normalize = transforms.Compose([
-        transforms.ToTensor(),  # Equivalent to ToTensorV2
+    imagenet_normalize = transforms.Compose([
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    int_normalize = transforms.Compose([
-        transforms.ToTensor(),  # Equivalent to ToTensorV2
+    pixel_normalize = transforms.Compose([
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0, 0, 0], std=[1, 1, 1]),
     ])
 
@@ -51,16 +61,16 @@ def torch_preprocess_transforms(image, input_transform, input_size):
     ## Transforms to an image for inference.
     if input_transform == 'ssd':    
         transformed_image = resize_transform(image)
-        transformed_image = float_normalize(transformed_image)
+        transformed_image = imagenet_normalize(transformed_image)
     elif input_transform == 'yolo':
         transformed_image = resize_center_color2_transform(image)
-        transformed_image = int_normalize(transformed_image)
+        transformed_image = pixel_normalize(transformed_image)
     elif input_transform == 'nanodet':
         transformed_image = resize_transform(image)
-        transformed_image = float_normalize(transformed_image)
+        transformed_image = imagenet_normalize(transformed_image)
     else: # default is for efficient det
         transformed_image = resize_center_color1_transform(image)
-        transformed_image = float_normalize(transformed_image)
+        transformed_image = imagenet_normalize(transformed_image)
     
     return sized_image, transformed_image
 
